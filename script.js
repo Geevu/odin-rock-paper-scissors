60e8b0df477e471acb60b5eba1af9a200d1e03fe
Geevu <atgeevu@gmail.com>
2025-05-21 14:41:40 +0530

    Add playRound function and varibales for result
    added playRound function which takes the output from the getChoice functions and compare them and parse the result to console
    
    assign varibales to the result of the getChoice functions

--- script.js
+++ script.js
@@ -1,19 +1,35 @@
+let humanScore = 0;
+let computerScore = 0;
+let computerChoice = getComputerChoice();
+let humanChoice = getHumanChoice();
+
+
 function getComputerChoice() {
   let choice = Math.floor(Math.random() * 100 - 1);
   if (choice <= 33) {
-    return "Rock";
+    return "rock";
   } else if (choice > 33 && choice <= 66) {
-    return "Paper";
+    return "paper";
   } else choice > 66 && choice <= 99;
-
-  return "Scissors";
+  return "scissors";
 }
 
-getComputerChoice();
-
-function getHumanChoice(){
-   let choice= prompt("Rock || Paper || Scissors")
-   choice = choice.toLowerCase
+function getHumanChoice() {
+  let choice = prompt("Rock || Paper || Scissors");
+  choice=choice.toLowerCase()
+  return choice;
 }
 
-getHumanChoice()
+function playRound(computerChoice, humanChoice) {
+  if (computerChoice === humanChoice) {
+    return console.log("It's a tie");
+  } else if (
+    (computerChoice == "rock" && humanChoice == "paper") ||
+    (computerChoice == "paper" && humanChoice == "scissors") ||
+    (computerChoice == "scissors" && humanChoice == "rock")
+  ) {
+    return console.log("You win");
+  } else return console.log("You lose");
+}
+console.log(playRound(computerChoice, humanChoice))
+
