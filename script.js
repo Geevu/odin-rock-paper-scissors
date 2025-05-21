9f263a3b312c3941258d40c50fca82d0b81dbdc8
Geevu <atgeevu@gmail.com>
2025-05-21 18:13:24 +0530

    add scoring mechanism and comments
    added scoring if else structure with comment and score adjustment alongside ensuring the console to log the choice of both players

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ let computerScore = 0;
 let computerChoice = getComputerChoice();
 let humanChoice = getHumanChoice();
 
-
 function getComputerChoice() {
   let choice = Math.floor(Math.random() * 100 - 1);
   if (choice <= 33) {
@@ -16,7 +15,7 @@ function getComputerChoice() {
 
 function getHumanChoice() {
   let choice = prompt("Rock || Paper || Scissors");
-  choice=choice.toLowerCase()
+  choice = choice.toLowerCase();
   return choice;
 }
 
@@ -25,11 +24,25 @@ function playRound(computerChoice, humanChoice) {
     return console.log("It's a tie");
   } else if (
     (computerChoice == "rock" && humanChoice == "paper") ||
-    (computerChoice == "paper" && humanChoice == "scissors") ||
+    (computerChoice == "paper" && humanChoice == "scissors" || "scissor") ||
     (computerChoice == "scissors" && humanChoice == "rock")
   ) {
     return console.log("You win");
   } else return console.log("You lose");
 }
-console.log(playRound(computerChoice, humanChoice))
+playRound(computerChoice, humanChoice)
+
+if (
+  (computerChoice == "rock" && humanChoice == "paper") ||
+  (computerChoice == "paper" && humanChoice == "scissors") ||
+  (computerChoice == "scissors" && humanChoice == "rock")
+) {
+  humanScore = humanScore + 1;
+  console.log("Your score is " + humanScore);
+} else if (computerChoice== humanChoice){;
+  console.log("No point for anyone")
+}else console.log (`Computer Score is ${computerScore +1} `)
+
+console.log("Computer choice was " + computerChoice)
+console.log("Your choice was " + humanChoice)
 
