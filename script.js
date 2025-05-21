19e4c2bdeed501879875be92d22db868c02060c6
Geevu <atgeevu@gmail.com>
2025-05-21 13:22:27 +0530

    Add function getComputerChoice and html boilerplate
    added the function getComputerChoice which will genreate a number between 00-99 and based on the number the value of rock or paper or scissor is assigned to the function

---
+++ script.js
@@ -0,0 +1,13 @@
+function getComputerChoice() {
+  let choice = Math.floor(Math.random() * 100 - 1);
+  if (choice <= 33) {
+    return "Rock";
+  } else if (choice > 33 && choice <= 66) {
+    return "Paper";
+  } else choice > 66 && choice <= 99;
+
+  return "Scissors";
+}
+
+getComputerChoice();
+
